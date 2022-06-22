927192d877c345c7ffc0660d6d55330bb96dbe60
antokh1v <antoxiv2001@mail.ru>
2022-06-22 11:00:54 +1000

    rebase Lesson_8

--- merch-shop/app/Enums/AbstractEnum.php
+++ merch-shop/app/Enums/AbstractEnum.php
@@ -4,11 +4,7 @@ namespace App\Enums;
 
 use ReflectionClass;
 
-<<<<<<< HEAD
 abstract class AbstractEnum
-=======
-class AbstractEnum
->>>>>>> Lesson 8: Add products and attributes (web & api)
 {
     public static function getConstants(){
         return (new ReflectionClass(static::class))->getConstants();
--- merch-shop/app/Enums/ProductAttributeType.php
+++ merch-shop/app/Enums/ProductAttributeType.php
@@ -2,11 +2,7 @@
 
 namespace App\Enums;
 
-<<<<<<< HEAD
 final class ProductAttributeType extends AbstractEnum
-=======
-class ProductAttributeType extends AbstractEnum
->>>>>>> Lesson 8: Add products and attributes (web & api)
 {
     public const STRING = 0;
     public const NUMERIC = 1;
